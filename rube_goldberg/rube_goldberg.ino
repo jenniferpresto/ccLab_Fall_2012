995dbb62f97e94d650fd69f00c86390dc3d7f5ce
Jennifer Presto <presj521@newschool.edu>
2012-10-23 09:21:04 -0400

    adjusted some delay times

--- rube_goldberg/rube_goldberg.ino
+++ rube_goldberg/rube_goldberg.ino
@@ -1,27 +1,35 @@
 /*
 *************************************************
-* Jennifer Presto                               *
-* CCLab Homework                                *
-* October 23, 2012                              *
-*                                               *
-* Assignment to create a Rube                   *
-* Goldberg machine:                             *
-* Button push triggers LED, which               *
-* triggers photosensor, which                   *
-* triggers motor, which knocks                  *
-* over dominoes, which trigger                  *
-* motion detector, which trigger                *
-* another LED for the big finish.               *
-*                                               *
-* Includes debounce code for the                *
-* button from the Arduino                       *
-* example folders, available here:              *
-*                                               *
-* http://www.arduino.cc/en//Tutorial/Debounce   *
-*                                               *
-*************************************************
-
-*/
+ * Jennifer Presto                               *
+ * CCLab Homework                                *
+ * October 23, 2012                              *
+ *                                               *
+ * Assignment to create a Rube                   *
+ * Goldberg machine:                             *
+ * Button push triggers LED, which               *
+ * triggers photosensor, which                   *
+ * triggers motor, which knocks                  *
+ * over dominoes, which trigger                  *
+ * motion detector, which trigger                *
+ * another LED for the big finish.               *
+ *                                               *
+ * Includes debounce code for the                *
+ * button from the Arduino                       *
+ * example folders, available here:              *
+ *                                               *
+ * http://www.arduino.cc/en//Tutorial/Debounce   *
+ *                                               *
+ * Circuit wired as follows:                     *
+ * Button, with 200 ohm resistor, to pin 2       *
+ * LED1, with 200 ohm resistor, to pin 12        *
+ * Photocell, with 10K ohm resistor, to pin A0   *
+ * Servo motor, no resistor, to pin 9            *
+ * Dominoes set off by motor, trip motion sensor *
+ * PIR motion detector, no resistor, to pin 4    *
+ *                                               *
+ *************************************************
+ 
+ */
 
 #include <Servo.h>
 
@@ -59,6 +67,7 @@ void setup(){
   delay(20000); //for motion sensor to warm up
   Serial.println("Ready!");
   littleMotor.attach(9); //attaches servo on pin 9 to the servo object
+
   //initialize all the pins
   pinMode(buttonPin, INPUT);
   pinMode(photoPin, INPUT);
@@ -69,7 +78,7 @@ void setup(){
 }
 
 void loop(){
-  
+
   //debounce code for button
   reading = digitalRead(buttonPin);
   if (reading != lastButtonState){
@@ -80,12 +89,12 @@ void loop(){
   }
   lastButtonState = reading;
   //end of debounce code
-  
+
   //when button pushed, flip boolean
   if (buttonState==1 && !buttonPush) {
     buttonPush = true;
   }
-  
+
   //ideally, when buttonPush boolean flips, would turn on the first LED
   //but abandoned steadily lit LED for now
   //because button mechanism unreliable (even with debounce code)
@@ -93,60 +102,58 @@ void loop(){
   if(buttonPush){
     digitalWrite(ledPin1, buttonState);
     lightOn = true;
+    delay(500);
   }
-  
+
   // when LED light comes on, reads photo sensor
   if(lightOn){
     photoSensorVal = analogRead(photoPin);
   }
-  
+
   // when photosensor is over 800 (LED makes it about 985, dim is at
   // about 735), trips photoTrip boolean
   if (photoSensorVal > 800){
     photoTrip = true;
+    delay(1000);
   }
-  
+
   // when photoTrip boolean is tripped, servo activated
-  // moves for one second, trips motorRun boolean
+  // moves for one-half second, trips motorRun boolean
   if (photoTrip && motorRun == false) {
-    motorPos = 100;
+    motorPos = 75;
     littleMotor.write(motorPos);
-    delay(3000);
+    delay(500);
     motorPos = 92;
     littleMotor.write(motorPos);
     motorRun = true;
+    delay(250);
   }
-  
-  // when motion sensor is tripped,
-  // read sensor and trips boolean
+
+  // when motorRun boolean is tripped,
+  // read motion sensor and trips boolean
+  // when dominoes fall in front of it
   if (motorRun) {
     pirSensorVal = digitalRead(4);
   }
-  
+
   if(pirSensorVal == 1){
     motionDetect = true;
+    delay(1000);
   }
-  
+
   if(motionDetect){
     digitalWrite(ledPin2, HIGH);
   }
-    
-  
-    
-  
-  
-  
 
-//  Serial.print(buttonState);
-//  Serial.print(" ");
-//  Serial.println(buttonPush);
-//Serial.print("photosensor: ");
-//Serial.println(photoSensorVal);
-//Serial.print("motor: ");
-//Serial.println(motorPos);
-Serial.print("motion sensor: ");
-Serial.println(pirSensorVal);
+  //  Uncomment below as desired for testing
+
+  //  Serial.print(buttonState);
+  //  Serial.print(" ");
+  //  Serial.println(buttonPush);
+  //  Serial.print("photosensor: ");
+  //  Serial.println(photoSensorVal);
+  //  Serial.print("motor: ");
+  //  Serial.println(motorPos);
+  //  Serial.print("motion sensor: ");
+  //  Serial.println(pirSensorVal);
 }
-
-
-
